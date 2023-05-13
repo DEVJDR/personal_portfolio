32f482c17ec1649d99e2d702f126558d3da2551f
ARUN <arunjdev26@gmail.com>
2023-05-13 18:23:16 +0530

    updated

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,5 +1,5 @@
 const header = {
-  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  homepage: 'https://DEVJDR.github.io/personal_portfolio',
   title: 'DEVJDR.',
 }
 
@@ -8,7 +8,7 @@ const about = {
   role: 'Front End Developer',
   description:
   'As a passionate web developer, I possess the technical prowess and expertise required to create visually stunning and high-performing websites that cater to the unique needs of your business. With my proficiency in web development frameworks, such as HTML, CSS, JavaScript, React.js I can effectively craft websites that are dynamic, responsive, and engaging.',
-  resume: 'https://example.com',
+  resume: 'https://drive.google.com/file/d/1L7VKD_h6AgVTnBfhmixi3dLS0nyjVsvT/view?usp=share_link',
   social: {
     linkedin: 'https://www.linkedin.com/in/arun-j-dev-150a241b5/',
     github: 'https://github.com/DEVJDR',
